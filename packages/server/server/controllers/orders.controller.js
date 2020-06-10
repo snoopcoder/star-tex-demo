75782184e63c1940153835af0fc64da8186dde36
Egor Kolbin <snoopcode@gmail.com>
2020-06-10 23:19:16 +0700

    feat: post orders api

--- packages/server/server/controllers/orders.controller.js
+++ packages/server/server/controllers/orders.controller.js
@@ -1,12 +1,22 @@
+const fs = require("fs");
+const strftime = require('strftime')
 /**
  * Create new order
  * @property {string} req.body.id - token.
  * @property {object} req.body.places - places to save.
  * @returns {boolean} result
  */
+// eslint-disable-next-line no-unused-vars
 function create(req, res, next) {
-
+  // eslint-disable-next-line node/prefer-promises/fs
+  fs.writeFile(
+    `output/${strftime("%Y%m%d-%H%M%S")}.txt`,
+    JSON.stringify(req.body.places),
+    function (error) {
+      if (error) console.log(error); // если возникла ошибка
+    }
+  );
+  res.send({ success: true });
 }
 
-
-module.exports = { create};
+module.exports = { create };
