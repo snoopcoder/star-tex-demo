d50930c9ca19030fddfbf36a1e4d2f84816662c6
Egor Kolbin <snoopcode@gmail.com>
2020-06-10 22:27:17 +0700

    feat: initial boilerplate commit

---
+++ packages/server/server/controllers/orders.controller.js
@@ -0,0 +1,12 @@
+/**
+ * Create new order
+ * @property {string} req.body.id - token.
+ * @property {object} req.body.places - places to save.
+ * @returns {boolean} result
+ */
+function create(req, res, next) {
+
+}
+
+
+module.exports = { create};
